4547d675f176104fa4d1460dd9d75dcaf71dbe1e
thiago-cloud <thiago.154ti@gmail.com>
2024-09-18 20:22:21 -0300

    appBurger-v1.0
    Implementação dos componentes Message, BurgerForm e Dashboard, além da estilização e operações de crud como getPedidos, promisse responsável pela requisição dos pedidos para mostrar os pedidos na tela, requisição dos status e requisição utilizando o método delete para exclusão dos pedidos.

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <div>
-            <form id="burger-form">
+            <Message :msg="msg" v-show="msg"/>
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="name">Nome do Cliente</label>
                     <input type="text" id="name" name="name" v-model="name" placeholder="Digite o seu nome"/>
@@ -20,13 +21,17 @@
                         <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
                     </select>
                 </div>
+
                 <div id="opcionais-container" class="input-container">
                     <label for="opcionais" id="opcionais-title">Selecione os opcionais:</label>
+                    
                     <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.id"/>
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo"/>
                         <span>{{ opcional.tipo }}</span>
                     </div>
+
                 </div>
+
                 <div class="input-container">
                     <input type="submit" class="submit-btn" value="Criar meu Burger!"/>
                 </div>
@@ -35,6 +40,8 @@
     </div>
 </template>
 <script>
+import Message from './Message.vue';
+
     export default{
         name:'BurgerForm',
         data(){
@@ -54,6 +61,10 @@
 
             }
         },
+        components: {
+            Message,
+        },
+
         methods:{
 
             //Requisição do backend
@@ -68,7 +79,48 @@
                 this.paes = data.paes;
                 this.carnes = data.carnes;
                 this.opcionaisdata = data.opcionais;
+                this.msg = data.msg;
             },
+
+            async createBurger(e){
+                e.preventDefault();
+                
+                //Os serão colocados dentro da variavel data via post após o submit
+                const data = {
+                    name: this.name,
+                    pao: this.pao,
+                    carne: this.carne,
+                    opcionais: Array.from(this.opcionais),
+                    status: "Solicitado"
+                }
+
+                //Pegando os dados do objeto data convertendo em string e passando para variável dataJson
+                const dataJson = JSON.stringify(data)
+
+                //Após pegarmos todos os dados via post agora e só fazer a requisição
+                const req = await fetch("http://localhost:3000/burgers",{
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: dataJson
+                })
+
+                const res = await req.json();
+                
+                //Mostrar mensagem após envio
+                this.msg = `Pedido Nº "${res.id}" solicitado com sucesso!`
+
+                //Limpar msg
+                setTimeout(()=> {
+                    this.msg = ""
+                }, 5000)
+
+                //Limpar campos
+                this.nome = ""
+                this.pao = ""
+                this.carne = ""
+                this.opcionais = []
+            
+            }
 
             
         },
@@ -100,7 +152,9 @@
 
     input, select{
         padding: 5px 10px;
-        width: 300px;
+        height:40px;
+        width: 400px;
+        border-radius: 5px;
     }
 
     #opcionais-container{
@@ -115,8 +169,24 @@
     .checkbox-container{
         display: flex;
         align-items:flex-start;
-        width:50%;
+        width:33%;
         margin-bottom: 20px;
+
+        transform: scale(1.2); /* Aumenta o tamanho */
+        -webkit-transform: scale(1.2); /* Compatibilidade com navegadores WebKit */
+        -moz-transform: scale(1.2); /* Compatibilidade com Firefox */
+        cursor: pointer; /* Cursor ao passar por cima */
+    }
+
+    .checkbox-container span{
+        display: block;
+        margin-top:12px;
+        margin-left:5px;
+    }
+
+    .checkbox-container div{
+        display: flex;
+        align-items: center;
     }
 
     .checkbox-container span,
@@ -139,10 +209,13 @@
         margin:0 auto;
         cursor:pointer;
         transition: .5s;
+        border-radius: 5px;
     }
 
     .submit-btn:hover{
         background-color: transparent;
         color:#222;
     }
+
+    
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,5 +16,6 @@ export default {
     Banner,
     BurgerForm
   }
+  
 }
 </script>
